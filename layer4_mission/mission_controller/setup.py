a9a1ef48031456fe3ea0a4d2142e0b08219ba572
IeokWai <j20041016@gmail.com>
2026-02-23 05:36:44 +0000

    mission

---
+++ layer4_mission/mission_controller/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+package_name = 'mission_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jon',
+    maintainer_email='s2577546@ed.ac.uk',
+    description='State machine',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+		'mission_controller = mission_controller.mission_controller_node:main'
+        ],
+    },
+)
